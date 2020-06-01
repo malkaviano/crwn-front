3e0e41470484b25b1acae6b0b1c3af393fede806
Rafael Karayannopoulos <rafael.kss@gmail.com>
2020-06-01 07:32:37 -0300

    fix sign out

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -26,7 +26,7 @@ const Header = ({ currentUser, hidden }) => {
                 {
                     currentUser ?
                         (
-                            <div className='option' onClick={auth.signOut}>
+                            <div className='option' onClick={() => auth.signOut()}>
                                 SIGN OUT
                             </div>
                         ) :
