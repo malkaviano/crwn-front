559f263ac1e5ae045741327283cff8a1daceadb5
Rafael Karayannopoulos <rafael.kss@gmail.com>
2020-06-07 07:17:09 -0300

    refactor

--- src/components/checkout/item.component.jsx
+++ src/components/checkout/item.component.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { withRouter } from 'react-router-dom';
 
 import './item.styles.scss';
 
